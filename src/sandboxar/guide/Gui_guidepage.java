b293d16404af80478aa870e586746d7f41cf4a56
Indy Haverkamp <Migushthe2nd@users.noreply.github.com>
2018-09-12 21:33:52 +0200

    Commit local branch

--- src/sandboxar/guide/Gui_guidepage.java
+++ src/sandboxar/guide/Gui_guidepage.java
@@ -112,8 +112,8 @@ public class Gui_guidepage extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        p1.main(null);
-    ?   // p.setVisable(false);
+        Gui_guidepage1.main(null);
+       // p.setVisable(false);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
