b864633ba03d7809520cf6eb5546b7a68eb716f5
Indy Haverkamp <Migushthe2nd@users.noreply.github.com>
2018-09-13 15:45:16 +0200

    changed to public static void main(String args[]) {

--- src/sandboxar/Gui.java
+++ src/sandboxar/Gui.java
@@ -18,7 +18,7 @@ public class Gui {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws InstantiationException, ClassNotFoundException, IllegalAccessException, UnsupportedLookAndFeelException {
+    public static void main() {
         //Checking if options.txt exist to prevent the 'Firt time opening' pop-up from popping up
         File f = new File(home + "/.ARSandboxTool/options.txt");
         if(f.exists() && !f.isDirectory()) 
