325d386cdd0c8bbdb1ae8368c12a2e193086ad97
Indy Haverkamp <Migushthe2nd@users.noreply.github.com>
2018-09-13 16:12:53 +0200

    Moved files to make source more structured

--- src/ARSandboxGUI/NotInstalled/Gui_guidepage.java
+++ src/ARSandboxGUI/NotInstalled/Gui_guidepage.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ARSandboxGUI.guide;
+package ARSandboxGUI.NotInstalled;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
