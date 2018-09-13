77be62a89ec1c558e1c6e6c0ee2a6ff1eed02aae
Indy Haverkamp <Migushthe2nd@users.noreply.github.com>
2018-09-13 19:46:03 +0200

    Name change

---
+++ src/ARSandboxGUI/NotInstalled/Gui_InstallingLoading.java
@@ -0,0 +1,84 @@
+package ARSandboxGUI.NotInstalled;
+
+
+public class Gui_InstallingLoading extends javax.swing.JFrame {
+
+    public Gui_InstallingLoading() {
+        initComponents();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        Text1 = new javax.swing.JLabel();
+        Text2 = new javax.swing.JLabel();
+        jProgressBar1 = new javax.swing.JProgressBar();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setCursor(new java.awt.Cursor(java.awt.Cursor.WAIT_CURSOR));
+        setResizable(false);
+
+        Text1.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        Text1.setText("Installing...");
+        Text1.setMaximumSize(new java.awt.Dimension(96, 16));
+        Text1.setMinimumSize(new java.awt.Dimension(96, 16));
+        Text1.setPreferredSize(new java.awt.Dimension(96, 16));
+
+        Text2.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        Text2.setText("Please be patient");
+
+        jProgressBar1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        jProgressBar1.setIndeterminate(true);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(28, 28, 28)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(Text1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(Text2)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(57, 57, 57)
+                        .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(66, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(Text1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(Text2)
+                .addGap(18, 18, 18)
+                .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(17, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) throws InterruptedException {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                final Gui_InstallingLoading gui = new Gui_InstallingLoading();
+                gui.setVisible(true);
+                gui.setTitle("Installation");
+                gui.pack();
+                gui.setLocationRelativeTo(null);
+                gui.setDefaultCloseOperation(Gui_InstallingLoading.EXIT_ON_CLOSE);
+            }
+        });
+    }
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Text1;
+    private javax.swing.JLabel Text2;
+    private javax.swing.JProgressBar jProgressBar1;
+    // End of variables declaration//GEN-END:variables
+
+}
+
